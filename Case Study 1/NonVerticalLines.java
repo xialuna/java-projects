f9a4ae5598745130e1501b6174faebaba0db5d9b
Xian Hui Cheng <xianhuicheng01@gmail.com>
2024-03-29 16:09:30 +0800

    Finish Non-vertical lines program

--- Case Study 1/NonVerticalLines.java
+++ Case Study 1/NonVerticalLines.java
@@ -12,7 +12,7 @@ public class NonVerticalLines {
 Choice: 1
 Prompts the user for the x-y coordinates of both points, inputs the four coordinates,
 and returns them to the calling function through output parameters. */
-    public static float[] get2_pt(){
+    public static float[][] get2_pt(){
         float[][] coords = new float[2][2];
         String[] input;
         for(int i = 0; i < 2; i++){
@@ -21,7 +21,7 @@ and returns them to the calling function through output parameters. */
             coords[i][0] = Float.parseFloat(input[0]);
             coords[i][1] = Float.parseFloat(input[1]);
         }
-        return new float[]{coords[0][0],coords[0][1],coords[1][0],coords[1][1]};
+        return coords;
     }
 
 /*
@@ -29,18 +29,15 @@ Choice 2:
  Prompts the user for the slope and x-y coordinates of the point, inputs the three 
 values and returns them to the calling function through output parameters.
  */
-    public static float get_pt_slope(){
-        float x,y;
-        String[] input;
+    public static float[] get_pt_slope(){
         System.out.print("Enter the slope=> ");
         float slope = in.nextFloat();
         in.nextLine();
         System.out.print("Enter the x-y coordinates of the point separated by a space=> ");
-        input = (in.nextLine()).split(" ");
-        x = Float.parseFloat(input[0]);
-        y = Float.parseFloat(input[1]);
-        
-        return x;//change
+        float x = in.nextFloat();
+        float y = in.nextFloat();
+
+        return new float[]{x,y,slope};
     }
 
 
@@ -56,32 +53,52 @@ values and returns them to the calling function through output parameters.
         return y - slope * x;
     }
 
-//Display Choice 1
+//Display Choice 1(Two-point form)
     public static void display2_pt(float p1_x, float p1_y, float p2_x, float p2_y){
         System.out.println("Two-point form");
-        System.out.printf("\t (%.2f - %.2f\n)", p2_y,p1_y);
-        System.out.println("m = ----------------");
-        System.out.printf("\t (%.2f - %.2f\n)",p2_x,p1_x);
-        // System.out.println("\nSlope-intercept form");
-        // System.out.printf("y = %.2fx + %.2f",get2_pt()[0],get2_pt()[1]);
-        
+        System.out.printf("\t(%.2f - %.2f)\n", p2_y,p1_y);
+        System.out.println("m = ----------------------");
+        System.out.printf("\t(%.2f - %.2f)\n",p2_x,p1_x);   
     }
     
 
     public static void display_pt_slope(float x, float y, float slope){
-        System.out.println("Point-slope form");
-        System.out.printf("\t y - %/2f = %.2f (x - .2f)\n)", x,slope,y);
+        System.out.println("\nPoint-slope form");
+        System.out.printf("y - %.2f = %.2f (x - %.2f) \n", x,slope,y);
     }
 
-    public static void display_slope_intcpt(float y, float slope){
-        System.out.println("Slope-intercept form");
-        System.out.printf("\t y = %.2fx - %.2f\n)", slope,y);
+    public static void display_slope_intcpt(float slope, float y_intercept){
+        System.out.println("\nSlope-intercept form");
+        System.out.printf("y = %.2fx + %.2f\n", slope,y_intercept);
     }
 
 
     
     public static void main(String[] args){
-        
+        char choice; 
+        do{
+            switch (get_problem()){
+                case 1:{
+                    in.nextLine();
+                    float[][] coords = get2_pt();
+                    float[] result = slope_intcpt_from2_pt(coords[0][0],coords[0][1], coords[1][0], coords[1][1]);
+                    display2_pt(coords[0][0],coords[0][1], coords[1][0], coords[1][1]);
+                    display_slope_intcpt(result[0], result[1]);
+                    break;
+                }case 2:{
+                    float[] coords = get_pt_slope();
+                    display_pt_slope(coords[0], coords[1], coords[2]);
+                    display_slope_intcpt(coords[2],intcpt_from_pt_slope(coords[0], coords[1], coords[2]));
+                    break;
+                }default:{
+                    break;
+                }
+            }
+
+            System.out.print("\nDo another conversion (Y or N)=> ");
+            choice = Character.toUpperCase(in.next().charAt(0));
+
+        }while(choice == 'Y');
        
     }
 }
--- Case Study 1/tempCodeRunnerFile.java
+++ Case Study 1/tempCodeRunnerFile.java
@@ -1,3 +1,1 @@
-int problemNum = in.nextInt();
-        // System.out.print("Enter rate in ml/hr =>");
-        // float rate = in.nextFloat();
+String[] input;

6e3ca632f956826e4106d5ed626da7ce5b987871
Xian Hui Cheng <xianhuicheng01@gmail.com>
2024-03-29 20:26:54 +0800

    Finish resistance

--- Case Study 1/Resistance.java
+++ Case Study 1/Resistance.java
@@ -1,3 +1,38 @@
+import java.util.Scanner;
 public class Resistance {
-    
+    private static Scanner in = new Scanner(System.in);
+    public static int search(String[] colorCode, int size, String target){
+        for(int i = 0; i < size; i++){
+            if(colorCode[i].equals(target)){
+                return i;
+            }
+        }return -1;
+    }
+    public static void main(String[] args){
+        char choice; 
+        String[] color_codes = {"black","brown", "red","orange","yellow","green","blue","violet","gray", "white"};
+        do{
+            System.out.println("\nEnter the colors of the resistor\'s three bands, beginning with the band nearest the end. Type the colors in lowercase letters only, NO CAPS.");
+            int[] bands = new int[3];
+            String invalidWord = "none";
+            for (int i = 0; i < 3; i++){
+                System.out.print("Band "+ (i+1) + " => ");
+                String target = in.nextLine().toLowerCase();
+                bands[i] = search(color_codes,color_codes.length,target);
+                if(bands[i] == -1)
+                    invalidWord = target;
+            }
+
+           if(!(invalidWord.equals("none"))){
+                System.out.println("Invalid color: "+ invalidWord);
+           }else{
+                double resistance = ((bands[0] * 10 + bands[1]) * Math.pow(10,bands[2]))/1000;
+                System.out.println("Resistance value: " + (int)resistance + " kilo-ohms");
+           }
+           
+            System.out.println("Do you want to decode another resistor?");
+            choice = Character.toLowerCase(in.next().charAt(0));
+            in.nextLine(); //get \n excess
+        }while(choice == 'y');
+    }
 }

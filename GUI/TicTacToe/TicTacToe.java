dab63c765614cf09f764aadb90df2231cf3f4dba
Xian Hui Cheng <xianhuicheng01@gmail.com>
2024-05-09 19:33:04 +0800

    Add starting tic tac toe game

---
+++ GUI/TicTacToe/TicTacToe.java
@@ -0,0 +1,37 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class TicTacToe {
+    JFrame frame =  new JFrame("Tic Tac Toe Game");;
+    JLabel textLabel = new JLabel("Tic Tac Toe");//default text
+    JPanel textPanel = new JPanel();
+
+    public TicTacToe(){
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(530,600);
+        frame.setVisible(true);
+
+        frame.setResizable(false); //prevent resizing of frame
+        frame.setLocationRelativeTo(null); //open frame on the middle of screen
+        frame.setLayout(new BorderLayout());
+        
+        ImageIcon image = new ImageIcon("D:\\Java Projects\\GUI\\TicTacToe\\logo.png");
+        frame.setIconImage(image.getImage());
+
+        textLabel.setForeground(new Color(0x50206C));//set color of text label
+        textLabel.setFont(new Font("Tahoma", Font.BOLD, 34)); //change text properties
+        textLabel.setHorizontalAlignment(JLabel.CENTER); //center text
+
+        textPanel.setLayout(new BorderLayout());
+        textPanel.add(textLabel); //add text label to text panel
+        frame.add(textPanel, BorderLayout.NORTH); //add text panel to frame and put it at the top
+        frame.validate(); // Validate the frame after adding components
+    }
+    
+
+    public static void main(String[] args){
+        TicTacToe ticTacToe = new TicTacToe();
+    }
+
+}

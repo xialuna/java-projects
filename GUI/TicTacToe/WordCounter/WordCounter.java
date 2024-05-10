4bbef4c9f0f6efe005e4939e950072150bb8d9f1
Xian Hui Cheng <xianhuicheng01@gmail.com>
2024-05-11 07:38:14 +0800

    Set up code for word counter

---
+++ GUI/TicTacToe/WordCounter/WordCounter.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class WordCounter {
+    public WordCounter(){
+        JFrame frame = new JFrame("Word Counter");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800,600);
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args){
+        WordCounter wordCounter = new WordCounter();
+        
+    }
+
+}

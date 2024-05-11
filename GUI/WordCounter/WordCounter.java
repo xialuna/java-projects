9ddbef556c7f7a958f2e50f4294a9c46cd41ed41
Xian Hui Cheng <xianhuicheng01@gmail.com>
2024-05-11 14:28:44 +0800

    Fix files for GUI

--- GUI/WordCounter/WordCounter.java
+++ GUI/WordCounter/WordCounter.java
@@ -7,13 +7,13 @@ public class WordCounter {
     public WordCounter(){
         JFrame frame = new JFrame("Word Counter");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(800,600);
+        frame.setSize(800,550);
         frame.setVisible(true);
     }
 
     public static void main(String[] args){
         WordCounter wordCounter = new WordCounter();
-        
+
     }
 
 }

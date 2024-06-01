d055b6a7e1ce56dd54567250b428a647e87978d4
Xian Hui Cheng <xianhuicheng01@gmail.com>
2024-06-01 11:33:46 +0800

    Set up layouts

--- FileHandling/TelephoneDirectory/Telephone.java
+++ FileHandling/TelephoneDirectory/Telephone.java
@@ -3,9 +3,40 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class Telephone extends JFrame {
+    JLabel lblHeading, lblName, lblAddress, lblTelephone;
+    TextField txtName, txtAddress, txtTelephone;
+    JPanel panelName, panelAddress, panelTelephone, panelButtons, panelCRUD, panelTable;
     Telephone(){
         setTitle("Telephone Directory");
-        setSize(600,600);
+        setSize(1212,700);
+        setResizable(false);
+        setLocationRelativeTo(null);
+
+        panelCRUD = new JPanel();
+        panelTable = new JPanel();
+        panelName = new JPanel();
+        panelAddress = new JPanel();
+        panelTelephone = new JPanel();
+        panelButtons = new JPanel();
+
+        panelCRUD.setLayout(new GridLayout(4,1,10,10));
+        panelTable.setLayout(new GridLayout(2,1,10,10));
+        panelName.setLayout(new GridLayout(2,1,5,5));;
+        panelAddress.setLayout(new GridLayout(2,1,5,5));
+        panelTelephone.setLayout(new GridLayout(2,1,5,5));
+        panelButtons.setLayout(new GridLayout(1,4,10,10));
+
+        Font fontDefault = new Font ("Microsoft JhengHei UI", Font.PLAIN, 16);
+        
+        lblName = new JLabel("Full Name: ");
+        lblName.setFont(fontDefault);
+        txtName = new TextField("", 20);
+        panelName.add(lblName);
+        panelName.add(txtName);
+        
+        panelCRUD.add(panelName);
+        add(panelCRUD);
+        // add(panelTable);
     }
     public static void main(String[] args){
         Telephone comic = new Telephone();

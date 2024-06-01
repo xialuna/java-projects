83af55b9a6ba511e10fb6549b273687446c9a6e7
Xian Hui Cheng <xianhuicheng01@gmail.com>
2024-06-01 10:40:20 +0800

    Set up starting code for telephone directory

--- FileHandling/TelephoneDirectory/Telephone.java
+++ FileHandling/TelephoneDirectory/Telephone.java
@@ -1,3 +1,16 @@
-public class Telephone {
-    
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Telephone extends JFrame {
+    Telephone(){
+        setTitle("Telephone Directory");
+        setSize(600,600);
+    }
+    public static void main(String[] args){
+        Telephone comic = new Telephone();
+        comic.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        comic.setVisible(true);
+
+    }
 }
